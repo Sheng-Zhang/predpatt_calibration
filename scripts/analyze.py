2022209cc0512698d0cf54303cb1fb193faef71c
Sheng Zhang <zhangsheng.zero@gmail.com>
2016-06-01 23:22:44 -0400

    Add code to calculate agreement in different metrics.

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import sys, csv, json
+import sys
+import csv
+import json
 from statistics import fleiss_kappa
 import itertools
-sys.path.insert(0, '/home/zhangsheng/.local/lib/python2.7/site-packages')
 import skll
+
 
 class Worker:
     """
@@ -16,16 +18,23 @@ class Worker:
         self.workerid = workerid
         self.answer = int(answer)
 
+
 class Predicate:
     """
     Define the predicate class.
 
     """
-    def __init__(self, pred_id, pred, question):
+    def __init__(self, hit_id, pred_id, pred, question, pprint=None):
+        self.hit_id = hit_id
         self.pred_id = pred_id
         self.pred_html = pred
-        self.workers = []
+        self.workers = {}
         self.question = question
+        if pprint:
+            pprint = pprint.replace('\\t', '\t')
+            pprint = pprint.replace('\\n', '\n')
+            self.pprint = pprint
+
 
 class Sentence:
     """
@@ -35,32 +44,31 @@ class Sentence:
 
     def __init__(self, q):
         self.sentid = q['sentenceID']
-        self.sentence_html = q['sentences']
+        self.sentence_html = q.get('sentence')  # q['sentence'])
         self.predicates = {}
 
-    def add_worker(self, q, row):
+    def add_worker(self, hit_id, q, row):
         # get predicate
         pred_id = q['pred_id']
         pred_html = q['predicate']
-        id_ = pred_id+pred_html
-        if id_ in self.predicates:
-            pred = self.predicates[id_]
-        else:
-            pred = Predicate(pred_id, q['predicate'], q)
+        id_ = pred_id + pred_html
+        if id_ not in self.predicates:
+            pred = Predicate(hit_id, pred_id, pred_html, q, q.get('pprint', None))
             self.predicates[id_] = pred
+        pred = self.predicates[id_]
 
         # get the worker number which is consistent with the question number
         i = q['questionID']
 
         # add worker to predicate
         workerid = row['WorkerId']
-        answer = row['Answer.correctness_'+i]
+        answer = row['Answer.correctness_' + i]
         worker = Worker(workerid, answer)
-        pred.workers.append(worker)
+        pred.workers[workerid] = worker
 
     def gen_answers(self):
         for pred in self.predicates.itervalues():
-            answers = [worker.answer for worker in pred.workers]
+            answers = [worker.answer for worker in pred.workers.itervalues()]
             distr = [0, 0]
             for answer in answers:
                 distr[answer] += 1
@@ -68,9 +76,9 @@ class Sentence:
 
     def gen_answer_pairs(self):
         for pred in self.predicates.itervalues():
-            answers = [worker.answer for worker in pred.workers]
+            answers = [worker.answer for worker in pred.workers.itervalues()]
             for s1, s2, in itertools.combinations(answers, 2):
-                yield [s1], [s2]
+                yield s1, s2
 
 
 def load_result(filepath):
@@ -79,32 +87,20 @@ def load_result(filepath):
         reader = csv.DictReader(csv_file)
         for i, row in enumerate(reader):
             json_var = json.loads(row['Input.json_variables'])
+            hit_id = row['Input.json_variables']
             for q in json_var:
+                if 'normal' not in q['pred_id']:
+                    continue
                 sentid = q['sentenceID']
                 # create sent object if it is not existed
                 if sentid not in results:
                     sent = Sentence(q)
                     results[sentid] = sent
-                else:
-                    sent = results[sentid]
+                sent = results[sentid]
                 # add worker
-                sent.add_worker(q, row)
+                sent.add_worker(hit_id, q, row)
     print len(results)
     return results
-
-def stratify_results(results):
-    ret = {5:[], 4:[], 3:[], 2:[], 1:[], 0:[]}
-    for sent in results.itervalues():
-        for pred, distr, _ in sent.gen_answers():
-            ret[distr[1]].append(pred.question)
-    for k, v in ret.iteritems():
-        print k, len(v)
-    with open('stratified.csv', 'wb') as f:
-        writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_ALL)
-        writer.writerow(['json_variables'])
-        for agreement in sorted(ret.keys(), key=lambda x: -x):
-            writer.writerow([json.dumps(ret[agreement], sort_keys=True)])
-
 
 
 def cal_fleiss_kappa(results):
@@ -114,27 +110,54 @@ def cal_fleiss_kappa(results):
             mat.append(distr)
     print len(mat)
     ret = fleiss_kappa(mat)
-    print "The fleiss_kappa value of all HITs is %f."%(ret)
+    print "The fleiss_kappa value of all HITs is %f." %(ret)
 
-def cal_cohen_kappa(results):
+
+def cal_pairwise_agreement(results):
     values = []
     for sent in results.itervalues():
         for s1, s2 in sent.gen_answer_pairs():
-            kappa_value = skll.kappa(s1, s2,
-                                     weights='quadratic')
-            values.append(kappa_value)
-    avg_kappa =  sum(values)/(len(values)+0.0)
-    print "The average kappa value of all HITs is %f."%(avg_kappa)
+            pair_agreement = 1 if s1 == s2 else 0
+            values.append(pair_agreement)
+    avg_kappa = sum(values) / (len(values) + 0.0)
+    print "The average pair-wise agreement of all HITs is %f." %(avg_kappa)
+
+
+def cal_cohen_kappa_by_hit(results):
+    # collect predicate by hit_id
+    preds_by_hit = {}
+    for sent in results.itervalues():
+        for pred in sent.predicates.itervalues():
+            if pred.hit_id not in preds_by_hit:
+                preds_by_hit[pred.hit_id] = []
+            preds_by_hit[pred.hit_id].append(pred)
+
+    kappas = []
+    # calculate kappa
+    for preds in preds_by_hit.itervalues():
+        # get answer list for each worker
+        worker_answers_by_hit = {worker_id: [] for worker_id
+                                 in preds[0].workers.keys()}
+        for pred in preds:
+            for worker in pred.workers.itervalues():
+                worker_answers_by_hit[worker.workerid].append(worker.answer)
+
+        # calculate kappa for each answer list pair
+        worker_answers = worker_answers_by_hit.values()
+        pairs_combinations = itertools.combinations(worker_answers, 2)
+        for l1, l2 in pairs_combinations:
+            kappa_value = skll.kappa(l1, l2)  #, weights='quadratic')
+            kappas.append(kappa_value)
+    avg_kappa = sum(kappas) / (len(kappas) + 0.0)
+    print len(preds_by_hit)
+    print "The average Cohen's kappa of all HITs is %f." %(avg_kappa)
+
 
 def main(filepath):
     results = load_result(filepath)
-    # from plot import boxplot
-    # boxplot(results)
-    stratify_results(results)
+    cal_pairwise_agreement(results)
     cal_fleiss_kappa(results)
-    cal_cohen_kappa(results)
+    cal_cohen_kappa_by_hit(results)
 
 if __name__ == '__main__':
     main(sys.argv[1])
-
-
